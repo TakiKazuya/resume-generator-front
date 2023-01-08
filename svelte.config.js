dc488ccdad2669f0fc2eeb657f3aab8fa33d7fe5
滝和弥 <kazup0689@gmail.com>
2023-01-08 10:54:30 +0900

    デプロイ設定

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,17 @@
-import firebase from "svelte-adapter-firebase";
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: firebase(),
+		adapter: adapter({
+			// default options are shown. On some platforms
+			// these options are set automatically — see below
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		})
 	}
 };
 
